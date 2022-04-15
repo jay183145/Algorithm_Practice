97e546e30ffcf2fce69351f7c19c045594de6959
jay183145 <jay183145@gmail.com>
2022-04-15 08:51:40 +0800

    pass the test!

--- 1.TwoSum/TwoSumClass.py
+++ 1.TwoSum/TwoSumClass.py
@@ -3,7 +3,12 @@ class Solution:
     def twoSum(self,nums,target):
         for i in nums:
             if target-i in nums[nums.index(i)+1:]:
-                return [nums.index(i),nums.index(target-i)]
+                if nums.index(i)==nums.index(target-i):
+                    return [nums.index(i),nums.index(i,nums.index(i)+1)]
+                else:    
+                    return [nums.index(i),nums.index(target-i)]
 
 print(Solution().twoSum([5,6,7,8,9],16))
 print(Solution().twoSum([3,2,4],6))
+print(Solution().twoSum([3,3],6))
+print(Solution().twoSum([3,2,3],6))
--- 1.TwoSum/TwoSumFun.py
+++ 1.TwoSum/TwoSumFun.py
@@ -1,9 +1,14 @@
 def twoSum(nums,target):
     for i in nums:
-        if target-i in nums[nums.index(i)+1:]:
-            return [nums.index(i),nums.index(target-i)]
+            if target-i in nums[nums.index(i)+1:]:
+                if nums.index(i)==nums.index(target-i):
+                    return [nums.index(i),nums.index(i,nums.index(i)+1)]
+                else:    
+                    return [nums.index(i),nums.index(target-i)]
 
 print(twoSum([5,6,7,8,9],16))
 print(twoSum([3,2,4],6))
+print(twoSum([3,3],6))
+print(twoSum([3,2,3],6))
 
 
